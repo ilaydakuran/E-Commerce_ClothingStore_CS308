2559d5ae9f2e9c2958a022cd5f9d891a8c2b192f
s21ilaydakuran <ilaydakuran@sabanciuniv.edu>
2021-06-10 23:15:20 +0300

    in profile page we can see the orders of the user

--- lib/routes/profile.dart
+++ lib/routes/profile.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:cs308_ecommerce/routes/catagories.dart';
 import 'package:cs308_ecommerce/routes/welcome.dart';
+import 'package:cs308_ecommerce/utils/color.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -96,6 +97,7 @@ class _profileState extends State<Profile> {
   }
   void initState() {
     super.initState();
+    _orderslist.clear();
     _getprofile();
     _orders();
   }
@@ -122,12 +124,18 @@ class _profileState extends State<Profile> {
     print(response.body);
     if (response.statusCode >= 200 && response.statusCode < 300) {
       //Successful transmission
-      List<dynamic> jsonMap = json.decode(response.body);
+      List<dynamic> jsonMap=json.decode(response.body);
+
+      //List<dynamic> jsonMap = json.decode(response.body);
       for (var entry in jsonMap)
       {
         for (var entry2 in entry) {
           print("${entry2}");
           print("ok");
+          setState(() {
+            _orderslist.add(Productorder.fromJson(entry2));
+
+          });
         }
       }
     }
@@ -158,76 +166,276 @@ class _profileState extends State<Profile> {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.purple[200],
       ),
-      body: Center(
+      body: Column(
+        children: [
+          Form(
+            key: _formKey,
+            child: Expanded(
 
-        child:Form(
-          key: _formKey,
-          child: ListView(
-              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0),
-              children: <Widget>[
+              child: Column(
+                // shrinkWrap: true,
+                //  padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0),
+                  children: <Widget>[
 
-                SizedBox(height: 60.0,),
-                TextFormField(
-                  autocorrect: true,
-                  keyboardType: TextInputType.streetAddress,
-                  decoration: InputDecoration(
-                    hintText: "Write your name and surname",
-                    errorStyle: TextStyle(
-                      fontSize: 13.0,
+                    SizedBox(height: 60.0,),
+                    TextFormField(
+                      autocorrect: true,
+                      keyboardType: TextInputType.streetAddress,
+                      decoration: InputDecoration(
+                        hintText: "Write your name and surname",
+                        errorStyle: TextStyle(
+                          fontSize: 13.0,
+                        ),
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                      onChanged: (value){
+                        setState(() {
+
+                        });
+                      },
                     ),
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                  onChanged: (value){
-                    setState(() {
+                    SizedBox(height: 40.0,),
+                    TextFormField(
+                      autocorrect: true,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: "Write your address...",
+                        errorStyle: TextStyle(
+                          fontSize: 13.0,
+                        ),
+                        prefixIcon: Icon(Icons.home_outlined),
+                      ),
+                      onChanged: (value){
+                        setState(() {
 
-                    });
-                  },
-                ),
-                SizedBox(height: 40.0,),
-                TextFormField(
-                  autocorrect: true,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    hintText: "Write your address...",
-                    errorStyle: TextStyle(
-                      fontSize: 13.0,
+                        });
+                      },
                     ),
-                    prefixIcon: Icon(Icons.home_outlined),
-                  ),
-                  onChanged: (value){
-                    setState(() {
-
-                    });
-                  },
-                ),
-                SizedBox(height: 40.0,),
-                TextFormField(
-                  obscureText: true,
-                  autocorrect: true,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    hintText: "Enter your card number",
-                    errorStyle: TextStyle(
-                      fontSize: 13.0,
+                    SizedBox(height: 40.0,),
+                    TextFormField(
+                      obscureText: true,
+                      autocorrect: true,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: "Enter your card number",
+                        errorStyle: TextStyle(
+                          fontSize: 13.0,
+                        ),
+                        prefixIcon: Icon(Icons.credit_card),
+                      ),
+                      onChanged: (value){
+                        setState(() {
+                        });
+                      },
                     ),
-                    prefixIcon: Icon(Icons.credit_card),
+
+                    /* Expanded(
+                   child: SizedBox(
+                     height: 1000,
+                     child: new ListView.builder(
+                        itemCount: _orderslist.length,
+                        itemBuilder: (context, i) {
+                          return new Card(
+                            child: new Column(
+                              children: [
+                              Row(
+                                children: [
+                                  new Image.network(_orderslist[i].image,),
+                                  new Text(_orderslist[i].name, style: TextStyle(color: Colors.black),),
+                                ],
+                              ),
+
+
+                                  Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 70,
+                                        child: Text("price: \$" + "${_orderslist[i].price}",
+                                          style: TextStyle(fontWeight: FontWeight.bold),),
+                                      ),
+                                      SizedBox(
+                                      width: 200,
+                                      child:Text("status: " + _orderslist[i].status,
+                                        style: TextStyle(fontWeight: FontWeight.bold),),
+
+                                      )//Text(_Prods[i].model,style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                  Text("delivery address: " + _orderslist[i].address,
+                                    style: TextStyle(fontWeight: FontWeight.bold),),
+
+                          ]
+                            ),
+                            margin: const EdgeInsets.all(0.0),
+                          );
+                        },
+                      ),
+                   ),
+                 )*/
+
+
+
+
+                  ]),
+            ),
+          ),
+          Expanded(
+
+            child: new ListView.builder(
+              itemCount: _orderslist.length,
+              itemBuilder: (context, i) {
+                print(_orderslist[i].status,);
+                return new Card(
+                  child: Column(
+                    children: [
+                      new ListTile(
+                        leading: new Image.network(_orderslist[i].image,), /*new CircleAvatar(backgroundImage: new NetworkImage(
+                                      // _searchResult[i].image,),),*/
+                        title: new Text(_orderslist[i].name, style: TextStyle(color: Colors.black),),
+                        subtitle: Row(
+                          children: [
+                            SizedBox(
+                              width: 300,
+                              child: Text("price: \$" + "${_orderslist[i].price}",
+                                style: TextStyle(fontWeight: FontWeight.bold),),
+                            ),
+
+                            //Text(_Prods[i].model,style: TextStyle(fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ),
+
+                    ],
                   ),
-                  onChanged: (value){
-                    setState(() {
-                    });
-                  },
-                ),
-                Container(
+                  margin: const EdgeInsets.all(0.0),
+                );
 
-                ),
+              },
+            ),
 
-
-
-              ]),
-        ),
+          )
+        ],
       ),
+
     );
   }
 
 
 }
+List<Productorder> _orderslist=[];
+class Productorder {
+  final int id, category_id, price, quantity_in_stocks, qty;
+  final String name, model, description, image, address, status, total;
+
+  Productorder({this.id, this.name, this.model, this.description, this.image, this.price, this.category_id, this.quantity_in_stocks,
+    this.address, this.status, this.total, this.qty});
+
+  factory Productorder.fromJson(Map<String, dynamic> json) {
+    return new Productorder(
+      id: json['id'],
+      name: json['name'],
+      model: json['model'],
+      description: json['description'],
+      image: json['image'],
+      price: json['price'],
+      category_id: json['category_id'],
+      quantity_in_stocks: json['quantity_in_stocks'],
+      address: json['address'],
+      status: json['status'],
+      total: json['total'],
+      qty: json['qty'],
+    );
+  }
+}
+/*
+class orders extends StatefulWidget {
+  @override
+  _ordersState createState() => _ordersState();
+}
+
+class _ordersState extends State<orders> {
+  Future<void> _orders() async {
+    final url = Uri.parse('http://10.0.2.2:8000/api/order');
+    /* var body = {
+      'call': 'catagories',
+      // 'categoryname': categoryname,
+    };*/
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String access= prefs.getString('accesstoken');
+    final response = await http.get(
+      Uri.http(url.authority, url.path),
+      headers:{HttpHeaders.contentTypeHeader: "application/json", HttpHeaders.authorizationHeader: "Bearer $access"},
+      /* headers: <String, String>{
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        'Authorization': 'Bearer $access',
+      },*/
+      //body: body,
+      // encoding: Encoding.getByName("utf-8"),
+    );
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      //Successful transmission
+      List<dynamic> jsonMap = json.decode(response.body);
+      for (var entry in jsonMap)
+      {
+        for (var entry2 in entry) {
+          print("${entry2}");
+          print("ok");
+          setState(() {
+            _orderslist.add(Productorder.fromJson(entry2));
+
+          });
+        }
+      }
+    }
+  }
+  void initState() {
+    super.initState();
+  //  _getprofile();
+    _orders();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( appBar: new AppBar(title: const Text("Orders"),backgroundColor: AppColors.primary,),
+        body:  Expanded(
+          child: SizedBox(
+            height: 1000,
+            child: new ListView.builder(
+              itemCount: _orderslist.length,
+              itemBuilder: (context, i) {
+                print(_orderslist[i].status,);
+                return new Card(
+                  child: Column(
+                    children: [
+                      new ListTile(
+                        leading: new Image.network(_orderslist[i].image,), /*new CircleAvatar(backgroundImage: new NetworkImage(
+                                  // _searchResult[i].image,),),*/
+                        title: new Text(_orderslist[i].name, style: TextStyle(color: Colors.black),),
+                        subtitle: Row(
+                          children: [
+                            SizedBox(
+                              width: 300,
+                              child: Text("price: \$" + "${_orderslist[i].price}",
+                                style: TextStyle(fontWeight: FontWeight.bold),),
+                            ),
+
+                            //Text(_Prods[i].model,style: TextStyle(fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  margin: const EdgeInsets.all(0.0),
+                );
+
+              },
+            ),
+          ),
+        )
+    );
+  }
+}
+*/
